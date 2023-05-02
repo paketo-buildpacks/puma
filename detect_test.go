d048f1832ad225976b6b8d62585c92e0b556c548
Forest Eckhardt <feckhardt@pivotal.io>
2023-05-02 16:31:30 +0000

    Adds a message to packit.Fail in detect

--- detect_test.go
+++ detect_test.go
@@ -84,7 +84,7 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 			_, err := detect(packit.DetectContext{
 				WorkingDir: workingDir,
 			})
-			Expect(err).To(MatchError(packit.Fail))
+			Expect(err).To(MatchError(packit.Fail.WithMessage("puma was not found in the Gemfile")))
 		})
 	})
 
